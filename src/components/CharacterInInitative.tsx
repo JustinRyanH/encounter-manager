8a8a721cb4a51ebecdc9e34f9bb59a1da7f2f9ed
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-12 16:27:48 -0500

    ref: Replace with group

--- src/components/CharacterInInitative.tsx
+++ src/components/CharacterInInitative.tsx
@@ -1,15 +1,14 @@
-import { Flex, Paper, Skeleton } from "@mantine/core";
+import { Flex, Group, Paper, Skeleton } from "@mantine/core";
 
 export function CharacterInInitative() {
     return (
         <Paper p="xl" shadow="md" withBorder>
-            <Flex gap="lg" align="center">
-                <Skeleton height={50} width={30} animate={false} />
+            <Group>
                 <Skeleton height={50} circle animate={false} />
                 <Skeleton height={50} width={100} animate={false} />
                 <Skeleton height={50} width={100} animate={false} />
                 <Skeleton height={50} width={100} animate={false} />
-            </Flex>
+            </Group>
         </Paper>
     );
 }
