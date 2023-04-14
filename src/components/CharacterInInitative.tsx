3e8c5568b86acabe2e1edf46317fc47a4c7f6e59
justinhurstwright <jryanhurst@gmail.com>
2023-04-14 16:19:58 -0500

    feat: Split top and bottom

--- src/components/CharacterInInitative.tsx
+++ src/components/CharacterInInitative.tsx
@@ -1,14 +1,35 @@
-import { Group, Paper, Skeleton } from "@mantine/core";
+import { Center, Container, Divider, Paper, SimpleGrid, Skeleton, Stack } from "@mantine/core";
 
 export function CharacterInInitative() {
     return (
         <Paper p="xl" shadow="md" withBorder>
-            <Group>
-                <Skeleton height={50} circle animate={false} />
-                <Skeleton height={50} width={100} animate={false} />
-                <Skeleton height={50} width={100} animate={false} />
-                <Skeleton height={50} width={100} animate={false} />
-            </Group>
+            <Container my="md">
+                <SimpleGrid cols={5} spacing="md">
+                    <Center>
+                        <Skeleton height={50} circle animate={false} />
+                    </Center>
+                    <Stack spacing="sm">
+                        <Skeleton height={20} animate={false} />
+                        <Divider />
+                        <Skeleton height={20} animate={false} />
+                    </Stack>
+                    <Stack spacing="sm">
+                        <Skeleton height={20} animate={false} />
+                        <Divider />
+                        <Skeleton height={20} animate={false} />
+                    </Stack>
+                    <Stack spacing="sm">
+                        <Skeleton height={20} animate={false} />
+                        <Divider />
+                        <Skeleton height={20} animate={false} />
+                    </Stack>
+                    <Stack spacing="sm">
+                        <Skeleton height={20} animate={false} />
+                        <Divider />
+                        <Skeleton height={20} animate={false} />
+                    </Stack>
+                </SimpleGrid>
+            </Container>
         </Paper>
     );
 }
