10a5c2b30f09dae0459148f4fdebd3e0279e1af4
justinhurstwright <jryanhurst@gmail.com>
2023-04-15 13:13:13 -0500

    refactor: Move to own file

--- src/components/CharacterInInitative.tsx
+++ src/components/CharacterInInitative.tsx
@@ -1,32 +1,12 @@
-import { Center, Container, Divider, Paper, SimpleGrid, Skeleton, Stack, TextInput, Title } from "@mantine/core";
+import { Center, Container, Paper, SimpleGrid, Skeleton } from "@mantine/core";
 import React from "react";
 import { InitiativeCharacter } from "../services/InititativeCharacter";
 import { ValueObserver } from "../services/ValueObserver";
-import { useWatchValueObserver } from "../hooks/watchValueObserver";
+import { SimpleStringAttribute } from "./SimpleStringAttribute";
 
-interface SimpleStringAttributeProps {
+export interface SimpleStringAttributeProps {
     title: string;
     observer: ValueObserver<string>
-}
-
-function SimpleStringAttribute({ title, observer }: SimpleStringAttributeProps): JSX.Element {
-    const [isEditing, setIsEditng] = React.useState(false);
-    const [value, setValue] = useWatchValueObserver(observer);
-    return (<Stack spacing="sm">
-        <Title order={3} align="center" transform="uppercase">{title}</Title>
-        <Divider />
-        <TextInput
-            onDoubleClick={() => setIsEditng(true)}
-            onBlur={() => setIsEditng(false)}
-            aria-label={title}
-            value={value}
-            onChange={(e) => setValue(e.currentTarget.value)}
-            placeholder={title}
-            radius="md"
-            styles={{ input: { textAlign: "center" } }}
-            readOnly={!isEditing}
-        />
-    </Stack>)
 }
 
 export function CharacterInInitative(): JSX.Element {
---
+++ src/components/SimpleStringAttribute.tsx
@@ -0,0 +1,23 @@
+import { Divider, Stack, TextInput, Title } from "@mantine/core";
+import React from "react";
+import { useWatchValueObserver } from "../hooks/watchValueObserver";
+import { SimpleStringAttributeProps } from "./CharacterInInitative";
+
+export function SimpleStringAttribute({ title, observer }: SimpleStringAttributeProps): JSX.Element {
+    const [isEditing, setIsEditng] = React.useState(false);
+    const [value, setValue] = useWatchValueObserver(observer);
+    return (<Stack spacing="sm">
+        <Title order={3} align="center" transform="uppercase">{title}</Title>
+        <Divider />
+        <TextInput
+            onDoubleClick={() => setIsEditng(true)}
+            onBlur={() => setIsEditng(false)}
+            aria-label={title}
+            value={value}
+            onChange={(e) => setValue(e.currentTarget.value)}
+            placeholder={title}
+            radius="md"
+            styles={{ input: { textAlign: "center" } }}
+            readOnly={!isEditing} />
+    </Stack>);
+}
