1983dba5a30f7d80f9c5df465cabebd059123a88
justinhurstwright <jryanhurst@gmail.com>
2023-04-15 17:38:18 -0500

    ref: Replace state

--- src/components/attributes/SimpleStringAttribute.tsx
+++ src/components/attributes/SimpleStringAttribute.tsx
@@ -1,20 +1,25 @@
 import { Divider, Stack, TextInput, Title } from "@mantine/core";
 import React from "react";
 
-import { useWatchValueObserver } from "~/hooks/watchValueObserver";
-
 import { SimpleAttributeProps } from "./SimpleAttributeProps";
+import {useAttribute} from "~/hooks/UseAttribute";
 
 
-export function SimpleStringAttribute({ title, observer }: SimpleAttributeProps<string>): JSX.Element {
-    const [isEditing, setIsEditng] = React.useState(false);
-    const [value, setValue] = useWatchValueObserver(observer);
+export function SimpleStringAttribute({ title, observer, cannotEdit }: SimpleAttributeProps<string>): JSX.Element {
+    const {
+        handleOnBlur,
+        handleOnDoubleClick,
+        isEditing,
+        setValue,
+        value,
+    } = useAttribute({ observer, cannotEdit });
+
     return (<Stack spacing="sm">
         <Title order={3} align="center" transform="uppercase">{title}</Title>
         <Divider />
         <TextInput
-            onDoubleClick={() => setIsEditng(true)}
-            onBlur={() => setIsEditng(false)}
+            onDoubleClick={handleOnDoubleClick}
+            onBlur={handleOnBlur}
             aria-label={title}
             value={value}
             onChange={(e) => setValue(e.currentTarget.value)}
