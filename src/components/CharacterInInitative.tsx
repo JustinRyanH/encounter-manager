6775866fad86ca0cc714b42448c8a0ead05d1123
justinhurstwright <jryanhurst@gmail.com>
2023-04-14 20:14:28 -0500

    feat: Allow to edit

--- src/components/CharacterInInitative.tsx
+++ src/components/CharacterInInitative.tsx
@@ -1,4 +1,5 @@
 import { Center, Container, Divider, Paper, SimpleGrid, Skeleton, Stack, TextInput, Title } from "@mantine/core";
+import React from "react";
 
 function AttributeSkeleton(): JSX.Element {
     return (<Stack spacing="sm">
@@ -15,17 +16,20 @@ interface NameAttributeProps {
 }
 
 function SimpleStringAttribute({ title, value = "", setValue = () => { } }: NameAttributeProps): JSX.Element {
+    const [isEditing, setIsEditng] = React.useState(false);
     return (<Stack spacing="sm">
         <Title order={3} align="center" transform="uppercase">{title}</Title>
         <Divider />
         <TextInput
+            onDoubleClick={() => setIsEditng(true)}
+            onBlur={() => setIsEditng(false)}
             aria-label={title}
             value={value}
             onChange={(e) => setValue(e.currentTarget.value)}
             placeholder={title}
             radius="md"
             styles={{ input: { textAlign: "center" } }}
-            disabled
+            readOnly={!isEditing}
         />
     </Stack>)
 }
