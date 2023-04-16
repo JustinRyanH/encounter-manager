e794fbcd1e805a134d89e3c0f014b8ceec0bdac3
justinhurstwright <jryanhurst@gmail.com>
2023-04-16 13:10:29 -0500

    feat: Add readOnly value

--- src/services/ValueObserver.ts
+++ src/services/ValueObserver.ts
@@ -1,5 +1,4 @@
 export interface ValueChangeMessageProps<T> {
-    observer: ValueObserver<T>;
     newValue: T;
     oldValue: T;
 }
@@ -7,14 +6,53 @@ export interface ValueChangeMessageProps<T> {
 export type ValueChangeMessage<T> = (props: ValueChangeMessageProps<T>) => void;
 
 /**
+ * Watches a ValueObserver and prevents subscribers from modifying the value.
+ */
+export class ReadonlyValueObserver<T> {
+    #observer: ValueObserver<T>;
+
+    constructor(observer: ValueObserver<T>) {
+        this.#observer = observer;
+    }
+
+    /**
+     * The current value of the publisher.
+     */
+    get value(): T {
+        return this.#observer.value;
+    }
+
+    /**
+     * Sets the value of the publisher and notifies all subscribers. 
+     */
+    add(subscriber: ValueChangeMessage<T>): void {
+        this.#observer.add(subscriber);
+    }
+
+    /**
+     * Removes a subscriber from the publisher.
+     * @param subscriber 
+     */
+    remove(subscriber: ValueChangeMessage<T>): void {
+        this.#observer.remove(subscriber);
+    }
+}
+
+/**
  * A class that can be used to publish a single value to multiple subscribers.
  */
 export class ValueObserver<T> {
+    #readonly: ReadonlyValueObserver<T>;
     #value: T;
     #subscribers: Array<ValueChangeMessage<T>> = [];
 
     constructor(value: T) {
         this.#value = value;
+        this.#readonly = new ReadonlyValueObserver(this);
+    }
+
+    get readonly(): ReadonlyValueObserver<T> {
+        return this.#readonly;
     }
 
     /**
@@ -30,7 +68,7 @@ export class ValueObserver<T> {
     set value(value: T) {
         const oldValue = this.#value;
         this.#value = value;
-        this.#subscribers.forEach((subscriber) => subscriber({ observer: this, newValue: value, oldValue }));
+        this.#subscribers.forEach((subscriber) => subscriber({ newValue: value, oldValue }));
     }
 
     /**
