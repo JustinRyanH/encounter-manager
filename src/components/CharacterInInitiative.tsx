869c2e1798fc022e233a31d5683afcf824013374
justinhurstwright <jryanhurst@gmail.com>
2023-04-15 19:50:21 -0500

    style: Replace with grid

--- src/components/CharacterInInitiative.tsx
+++ src/components/CharacterInInitiative.tsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { Center, Container, Paper, SimpleGrid, Skeleton } from "@mantine/core";
+import {Center, Container, Grid, Paper, SimpleGrid, Skeleton} from "@mantine/core";
 
-import { InitiativeCharacter } from "~/services/InititativeCharacter";
+import {InitiativeCharacter} from "~/services/InititativeCharacter";
 
-import { SimpleStringAttribute, SimpleNumberAttribute } from "./attributes";
+import {SimpleStringAttribute, SimpleNumberAttribute} from "./attributes";
 import {HitPointsAttribute} from "~/components/attributes/HitPointsAttribute";
 
 export function CharacterInInitiative(): JSX.Element {
-    const character = React.useMemo(() => new InitiativeCharacter({ name: 'Name', initiative: 10 }), []);
+    const character = React.useMemo(() => new InitiativeCharacter({name: 'Temp Name', initiative: 10}), []);
     return (
         <Paper p="xl" shadow="md" withBorder>
-            <Container>
-                <SimpleGrid cols={4} spacing="md">
-                    <Center>
-                        <Skeleton height={50} circle animate={false} />
-                    </Center>
-                    <SimpleStringAttribute title="Name" observer={character.nameObserver} />
-                    <SimpleNumberAttribute title="Initiative" observer={character.initiativeObserver} cannotEdit />
-                    <HitPointsAttribute />
-                </SimpleGrid>
-            </Container>
+            <Grid align="flex-end">
+                <Grid.Col span={1}>
+                    <Skeleton height={50} circle animate={false}/>
+                </Grid.Col>
+                <Grid.Col span={2}>
+                    <SimpleStringAttribute title="Name" observer={character.nameObserver}/>
+                </Grid.Col>
+                <Grid.Col span={2}>
+                    <SimpleNumberAttribute title="Initiative" observer={character.initiativeObserver} cannotEdit/>
+                </Grid.Col>
+                <Grid.Col span={5}>
+                    <HitPointsAttribute/>
+                </Grid.Col>
+            </Grid>
         </Paper>
     );
 }
