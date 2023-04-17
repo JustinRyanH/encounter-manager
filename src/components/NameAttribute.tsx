9617f63037ac20d1ce47aba49c92890a62128e85
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-16 21:50:29 -0500

    ref: Move component up scope

--- src/components/NameAttribute.tsx
+++ src/components/NameAttribute.tsx
@@ -1,11 +1,50 @@
 import React from "react";
-import { ActionIcon, Flex, FocusTrap, Popover, Text, TextInput, UnstyledButton } from "@mantine/core";
+import { ActionIcon, Flex, Popover, Text, TextInput, UnstyledButton } from "@mantine/core";
 import { IconCheck } from "@tabler/icons-react";
 import { useDisclosure } from "@mantine/hooks";
 
 import { useWatchValueObserver } from "~/hooks/watchValueObserver";
 import { Attribute } from "~/components/Attribute";
 import { InitiativeCharacter } from "~/services/InititativeCharacter";
+
+
+function NameAttributeEdit({ character, handles }: { character: InitiativeCharacter, handles: { readonly close: () => void } }): JSX.Element {
+    const ref = React.useRef<HTMLDivElement>(null);
+    const [newName, setNewName] = React.useState('');
+    const handleSetNewName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewName(e.target.value);
+    }
+    const commitNewName = () => {
+        if (newName !== '') {
+            character.name = newName;
+            setNewName('');
+        }
+        handles.close();
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Escape') {
+            handles.close();
+            return;
+        }
+        if (e.key === 'Enter') {
+            commitNewName();
+        }
+    }
+
+    const handleBlur = (e: React.FocusEvent<HTMLInputElement | HTMLButtonElement>) => {
+        if (!ref.current?.contains(e.relatedTarget as Node)) {
+            handles.close();
+        }
+    }
+
+    return <Flex ref={ref} align="center" gap="xs">
+        <TextInput onKeyDown={handleKeyDown} placeholder="Update Character Name" value={newName} onChange={handleSetNewName} onBlur={handleBlur} />
+        <ActionIcon title="Commit" onClick={commitNewName} onBlur={handleBlur}>
+            <IconCheck size="1.75rem" />
+        </ActionIcon>
+    </Flex>;
+}
 
 
 export function NameAttribute({ character }: { character: InitiativeCharacter }): JSX.Element {
@@ -32,42 +71,4 @@ export function NameAttribute({ character }: { character: InitiativeCharacter })
             </Popover.Dropdown>
         </Popover>
     );
-
-    function NameAttributeEdit({ character, handles }: { character: InitiativeCharacter, handles: { readonly close: () => void } }): JSX.Element {
-        const ref = React.useRef<HTMLDivElement>(null);
-        const [newName, setNewName] = React.useState('');
-        const handleSetNewName = (e: React.ChangeEvent<HTMLInputElement>) => {
-            setNewName(e.target.value);
-        }
-        const commitNewName = () => {
-            if (newName !== '') {
-                character.name = newName;
-                setNewName('');
-            }
-            handles.close();
-        }
-
-        const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-            if (e.key === 'Escape') {
-                handles.close();
-                return;
-            }
-            if (e.key === 'Enter') {
-                commitNewName();
-            }
-        }
-
-        const handleBlur = (e: React.FocusEvent<HTMLInputElement | HTMLButtonElement>) => {
-            if (!ref.current?.contains(e.relatedTarget as Node)) {
-                handles.close();
-            }
-        }
-
-        return <Flex ref={ref} align="center" gap="xs">
-            <TextInput onKeyDown={handleKeyDown} placeholder="Update Character Name" value={newName} onChange={handleSetNewName} onBlur={handleBlur} />
-            <ActionIcon title="Commit" onClick={commitNewName} onBlur={handleBlur}>
-                <IconCheck size="1.75rem" />
-            </ActionIcon>
-        </Flex>;
-    }
 }
