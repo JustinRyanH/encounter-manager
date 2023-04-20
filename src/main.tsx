e4f3431731b8d4940d41ec030d8349093db841b2
justinhurstwright <jryanhurst@gmail.com>
2023-04-20 17:34:16 -0500

    feat: Add notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-            colorScheme: 'dark',
-            colors: {
-                danger: [
-                    'rgba(255, 36, 36, 0.1)',
-                    'rgba(255, 36, 36, 0.2)',
-                    'rgba(255, 36, 36, 0.4)',
-                    'rgba(255, 36, 36, 0.6)',
-                    'rgba(255, 36, 36, 0.8)',
-                    'rgba(255, 36, 36, 1.0)',
-                ],
-                alphaDark: [
-                    'rgba(26, 27, 30, 0.1)',
-                    'rgba(26, 27, 30, 0.2)',
-                    'rgba(26, 27, 30, 0.4)',
-                    'rgba(26, 27, 30, 0.6)',
-                    'rgba(26, 27, 30, 0.8)',
-                    'rgba(26, 27, 30, 1.0)',
-                ]
-            },
-    }}
-    >
-        <App />
-    </MantineProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{
+                colorScheme: 'dark',
+                colors: {
+                    danger: [
+                        'rgba(255, 36, 36, 0.1)',
+                        'rgba(255, 36, 36, 0.2)',
+                        'rgba(255, 36, 36, 0.4)',
+                        'rgba(255, 36, 36, 0.6)',
+                        'rgba(255, 36, 36, 0.8)',
+                        'rgba(255, 36, 36, 1.0)',
+                    ],
+                    alphaDark: [
+                        'rgba(26, 27, 30, 0.1)',
+                        'rgba(26, 27, 30, 0.2)',
+                        'rgba(26, 27, 30, 0.4)',
+                        'rgba(26, 27, 30, 0.6)',
+                        'rgba(26, 27, 30, 0.8)',
+                        'rgba(26, 27, 30, 1.0)',
+                    ]
+                },
+            }}
+        >
+            <Notifications autoClose={4000} />
+            <App />
+        </MantineProvider>
+    </React.StrictMode>
 );
