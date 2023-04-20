9d588fd2fb5d7117fc20351f4a9f4256f0f3136d
justinhurstwright <jryanhurst@gmail.com>
2023-04-20 17:12:02 -0500

    ref: Extract hook

--- src/components/encounter/DisplayEncounter.tsx
+++ src/components/encounter/DisplayEncounter.tsx
@@ -6,9 +6,13 @@ import { EncounterCharacter } from "~/components/encounter/EncounterCharacter";
 import { useEncounterContext } from "~/components/encounter/EncounterContext";
 import { useStyles } from "~/components/encounter/DisplayEncounter.styles";
 
-export function DisplayEncounter() {
+/**
+ * A hook that returns the expanded characters and a function to update the expanded characters.
+ * 
+ * @returns [expanded, setExpanded]
+ */
+function useExpandedEncounter() {
     const encounter = useEncounterContext();
-    const characters = useWatchValueObserver(encounter.charactersObserver);
 
     const activeCharacter = useWatchValueObserver(encounter.activeCharacterObserver);
     const [userOpenedCharacters, setUserOpenedCharacter] = React.useState<string[]>([]);
@@ -23,16 +27,18 @@ export function DisplayEncounter() {
         if (activeCharacter) setUserOpenedCharacter([activeCharacter.id]);
     }, [activeCharacter]);
 
+
+    return [userOpenedCharacters, updateCharacters] as const;
+}
+
+export function DisplayEncounter() {
+    const encounter = useEncounterContext();
+    const characters = useWatchValueObserver(encounter.charactersObserver);
+
+    const [expanded, setExpanded] = useExpandedEncounter();
     const { classes } = useStyles();
 
-    return (<Accordion
-        value={userOpenedCharacters}
-        onChange={updateCharacters}
-        classNames={classes}
-        chevronPosition="left"
-        variant="separated"
-        multiple
-    >
+    return (<Accordion value={expanded} onChange={setExpanded} classNames={classes} chevronPosition="left" variant="separated" multiple>
         {characters.map((c) => <EncounterCharacter character={c} key={c.id} />)}
     </Accordion>);
 }
