813ff0065f6daee22ff6cc9d0385013d0b0904ca
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-22 16:46:37 -0500

    feat: Style the form

--- src/components/encounter/AddCharacterToEncounter.tsx
+++ src/components/encounter/AddCharacterToEncounter.tsx
@@ -1,36 +1,46 @@
 import React from "react";
-import { Button, Group, NumberInput, Stack, TextInput, Title } from "@mantine/core";
+import { Button, CSSObject, Group, NumberInput, Stack, TextInput, Title } from "@mantine/core";
 import { useForm } from "@mantine/form";
 
 import { Encounters } from "~/services/Encounters";
 
+interface EncounterFormProps {
+    name: string,
+    initiative: number | '',
+    totalHp: number | '',
+    tmpHp: number | '',
+}
+
 export function AddCharacterToEncounter({ }: { encounter: Encounters; }) {
-    const form = useForm({
+    const form = useForm<EncounterFormProps>({
         initialValues: {
             name: '',
-            initiative: 0,
-            totalHp: 0,
-            tmpHp: 0,
+            initiative: '',
+            totalHp: '',
+            tmpHp: '',
         },
     });
 
 
+    const input: CSSObject = { textAlign: 'center' };
+    const label: CSSObject = { width: '100%', textAlign: 'center' };
     return (
         <form onSubmit={form.onSubmit((values) => console.log(values))}>
             <Stack>
                 <Title order={4}>Add Character</Title>
 
-                <TextInput label="Name" {...form.getInputProps('name')} />
-                <NumberInput label="Initiative" hideControls {...form.getInputProps('initiative')} />
+                <TextInput styles={{ input, label }} label="Name" required {...form.getInputProps('name')} />
+                <NumberInput styles={{ input, label }} label="Initiative" hideControls required {...form.getInputProps('initiative')} />
                 <Group position="apart" grow>
                     <NumberInput
-                        styles={{ input: { textAlign: 'center' }, label: { width: '100%', textAlign: 'center' } }}
+                        styles={{ input, label}}
                         label="Total HP"
                         hideControls
+                        required
                         {...form.getInputProps('totalHp')}
                     />
                     <NumberInput
-                        styles={{ input: { textAlign: 'center' }, label: { width: '100%', textAlign: 'center' } }}
+                        styles={{ input, label }}
                         label="Temporary HP"
                         hideControls
                         {...form.getInputProps('tmpHp')}
