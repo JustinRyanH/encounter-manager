75fed59216596577ce5785bf2e15d4c48525c3cc
justinhurstwright <jryanhurst@gmail.com>
2023-04-22 14:25:41 -0500

    feat: Add character

--- src/components/encounter/AddCharacterToEncounter.tsx
+++ src/components/encounter/AddCharacterToEncounter.tsx
@@ -1,9 +1,43 @@
 import React from "react";
-import { Stack, Title } from "@mantine/core";
+import { Button, Group, NumberInput, Stack, TextInput, Title } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
 import { Encounters } from "~/services/Encounters";
 
 export function AddCharacterToEncounter({ }: { encounter: Encounters; }) {
-    return (<Stack>
-        <Title order={4}>Add Character</Title>
-    </Stack>);
+    const form = useForm({
+        initialValues: {
+            name: '',
+            initiative: 0,
+            totalHp: 0,
+            tmpHp: 0,
+        },
+    });
+
+
+    return (
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <Stack>
+                <Title order={4}>Add Character</Title>
+
+                <TextInput label="Name" {...form.getInputProps('name')} />
+                <NumberInput label="Initiative" hideControls {...form.getInputProps('initiative')} />
+                <Group position="apart" grow>
+                    <NumberInput
+                        styles={{ input: { textAlign: 'center' }, label: { width: '100%', textAlign: 'center' } }}
+                        label="Total HP"
+                        hideControls
+                        {...form.getInputProps('totalHp')}
+                    />
+                    <NumberInput
+                        styles={{ input: { textAlign: 'center' }, label: { width: '100%', textAlign: 'center' } }}
+                        label="Temporary HP"
+                        hideControls
+                        {...form.getInputProps('tmpHp')}
+                    />
+                </Group>
+                <Button type="submit">Add</Button>
+            </Stack >
+        </form >
+    );
 }
