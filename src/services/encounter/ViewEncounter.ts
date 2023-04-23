51b02a0023d35c9a496f44e965ac4cbdf5a7f671
justinhurstwright <jryanhurst@gmail.com>
2023-04-23 12:30:34 -0500

    ref: Swap to Observer

--- src/services/encounter/ViewEncounter.ts
+++ src/services/encounter/ViewEncounter.ts
@@ -1,23 +1,30 @@
 import { Encounter } from "~/services/encounter/Encounter";
 import { ActiveCharacter } from "~/services/encounter/ActiveCharacter";
+import { ValueChangeMessageProps, ValueObserver } from "~/services/ValueObserver";
 
 export class ViewEncounter {
     #encounter: Encounter;
-    #openededCharacters: Array<ActiveCharacter> = [];
+    #openededCharacters = new ValueObserver<ActiveCharacter[]>([]);
 
     constructor({ encounter }: { encounter: Encounter }) {
         this.#encounter = encounter;
+        this.#encounter.activeCharacterObserver.add(this.onChangeActiveCharacter);
     }
 
     get openedCharacters(): Array<ActiveCharacter> {
-        if (!this.#encounter.activeCharacter) return this.#openededCharacters;
-        return [this.#encounter.activeCharacter, ...this.#openededCharacters];
+        return this.#openededCharacters.value;
     }
 
     open(characterId: string): void {
         const character = this.#encounter.characters.find(character => character.id === characterId);
         if (character) {
-            this.#openededCharacters = [...this.#openededCharacters, character];
+            this.#openededCharacters.value = [...this.openedCharacters, character];
+        }
+    }
+
+    private onChangeActiveCharacter = ({ newValue }: ValueChangeMessageProps<ActiveCharacter | null>) => {
+        if (newValue) {
+            this.#openededCharacters.updateValue([newValue, ...this.openedCharacters]);
         }
     }
 }
