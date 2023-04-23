622343fe5f8538ba3bc0936e688e93c8e9e26772
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-23 00:11:01 -0500

    feat: introduce activeCharacters

---
+++ src/services/encounter/ViewEncounter.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from 'vitest';
+
+import { ActiveCharacter } from "~/services/encounter/ActiveCharacter";
+import { Encounter } from "~/services/encounter/Encounter";
+import { ViewEncounter } from "~/services/encounter/ViewEncounter";
+
+
+describe('ViewEncounter', () => {
+    describe('openedCharacters', () => {
+        test('returns the active character', () => {
+            const characterA = new ActiveCharacter({ name: 'A', initiative: 10 });
+            const characterB = new ActiveCharacter({ name: 'B', initiative: 5 });
+
+            const encounter = new Encounter({ characters: [characterA, characterB] })
+            const viewEncounter = new ViewEncounter({ encounter });
+
+            encounter.startEncounter();
+            expect(viewEncounter.openedCharacters).toEqual([characterA]);
+        });
+
+        test('returns an empty array if there is no active character', () => {
+            const characterA = new ActiveCharacter({ name: 'A', initiative: 10 });
+            const characterB = new ActiveCharacter({ name: 'B', initiative: 5 });
+
+            const encounter = new Encounter({ characters: [characterA, characterB] })
+            const viewEncounter = new ViewEncounter({ encounter });
+
+            expect(encounter.activeCharacter).toEqual(null);
+            expect(viewEncounter.openedCharacters).toEqual([]);
+        });
+    });
+});
--- src/services/encounter/ViewEncounter.ts
+++ src/services/encounter/ViewEncounter.ts
@@ -1,13 +1,15 @@
 import { Encounter } from "~/services/encounter/Encounter";
-import { ReadonlyValueObserver } from "~/services/ValueObserver";
 import { ActiveCharacter } from "~/services/encounter/ActiveCharacter";
 
-class ViewEncounter {
+export class ViewEncounter {
     #encounter: Encounter;
-    #activeCharacterObserver: ReadonlyValueObserver<ActiveCharacter | null>;
 
     constructor({ encounter }: { encounter: Encounter }) {
         this.#encounter = encounter;
-        this.#activeCharacterObserver = this.#encounter.activeCharacterObserver;
+    }
+
+    get openedCharacters(): Array<ActiveCharacter> {
+        if (!this.#encounter.activeCharacter) return [];
+        return [this.#encounter.activeCharacter];
     }
 }
