2c2e0fd3185f68aba922175f8e25141853ac93fe
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-22 23:27:43 -0500

    feat: Allow stop/starting encounters

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ function App() {
             <Accordion.Control>Manager Encounter</Accordion.Control>
             <Accordion.Panel>
               <Stack>
+                <Button onClick={() => encounter.startEncounter()}>Start Encounter</Button>
                 <Button color="gray" onClick={() => encounter.stopEncounter()}>Stop Encounter</Button>
               </Stack>
             </Accordion.Panel>
