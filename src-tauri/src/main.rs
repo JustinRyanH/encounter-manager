7b7476162813d04e60468dd47f1ba273e0759586
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-24 11:16:08 -0500

    spike: Initial files

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::api::path::document_dir;
+
+const ENCOUNTER_MANAGER_DIRECTORY: &'static str = "Encounter Manager";
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn browse_document_files() -> Result<Vec<String>, String> {
+    let mut path = document_dir().ok_or("Could not find Document Directory")?;
+    path.push(ENCOUNTER_MANAGER_DIRECTORY);
+    if (!path.clone().exists()) {
+        return Err("Could not find Encounter Manager Directory".to_string());
+    }
+    Ok(["Test File Name".to_string()].to_vec())
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, browse_document_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/FileExperiment.tsx
+++ src/components/FileExperiment.tsx
@@ -1,4 +1,11 @@
-const ENCOUNTER_MANAGER_FOLDER = 'Encounter Manager';
+import React from "react";
+import { invoke } from '@tauri-apps/api';
+
 export function FileExperiment() {
+    React.useEffect(() => {
+        invoke('browse_document_files')
+            .then((result) => console.log({ browse_document_files: result }))
+            .catch((error) => console.error(error));
+    }, []);
     return <p>Manage Files</p>;
 }
