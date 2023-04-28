66030944ba92e5e9dafd6f565d97436377059a66
justinhurstwright <jryanhurst@gmail.com>
2023-04-27 19:46:38 -0500

    ref: Move to struct

--- src-tauri/src/services/data.rs
+++ src-tauri/src/services/data.rs
@@ -1,9 +1,34 @@
-use std::sync::Arc;
+use std::{path::Path, sync::Arc};
 
+use notify::{RecursiveMode, Watcher};
 use serde::Serialize;
-use notify::{Watcher, RecursiveMode};
 use tauri::{async_runtime, Manager, Runtime, State, Wry};
 use tokio::sync::Mutex;
+
+struct FileWatcher {
+    pub watcher: notify::RecommendedWatcher,
+}
+
+impl FileWatcher {
+    pub fn new() -> Result<Self, String> {
+        let watcher = notify::recommended_watcher(|res| {
+            match res {
+                Ok(event) => {
+                    println!("event: {:?}", event);
+                }
+                Err(e) => println!("watch error: {:?}", e),
+            };
+        })
+        .map_err(|e| e.to_string())?;
+        Ok(Self { watcher })
+    }
+
+    pub fn watch(&mut self, path: &Path) -> Result<(), String> {
+        self.watcher
+            .watch(path, RecursiveMode::Recursive)
+            .map_err(|e| e.to_string())
+    }
+}
 
 #[derive(Clone)]
 pub struct BackgroundData<R: Runtime> {
@@ -21,34 +46,26 @@ impl ArcData {
     }
 
     pub fn start_main_loop(self) -> Result<(), String> {
-        let mut watcher = notify::recommended_watcher(move |res| {
-            match res {
-                Ok(event) => {
-                    println!("event: {:?}", event);
-                }
-                Err(e) => println!("watch error: {:?}", e),
-            };
-        }).map_err(|e| e.to_string())?;
+        let mut file_watcher = FileWatcher::new()?;
 
         async_runtime::spawn(async move {
-            let watch_path = {
-                let mut path = tauri::api::path::document_dir().expect("Could not find Document Directory");
-                path.push("Encounter Manager");
-                if !path.clone().exists() {
-                    std::fs::create_dir(path.clone()).expect("Could not create directory");
-                }
-                path
-            };
+            let watch_path = get_or_create_doc_path();
 
-            watcher.watch(watch_path.as_path(), RecursiveMode::Recursive).expect("Could not watch directory");
+            file_watcher
+                .watch(watch_path.as_path())
+                .expect("Could not watch directory");
+
             loop {
                 let data = self.0.lock().await;
-                data.app_handle.emit_all("test",
-                              &ExampleStruct {
-                                  name: "test".to_string(),
-                                  age: 42,
-                              },
-                ).expect("failed to emit");
+                data.app_handle
+                    .emit_all(
+                        "test",
+                        &ExampleStruct {
+                            name: "test".to_string(),
+                            age: 42,
+                        },
+                    )
+                    .expect("failed to emit");
                 tokio::time::sleep(std::time::Duration::from_secs(1)).await;
             }
         });
@@ -56,6 +73,17 @@ impl ArcData {
     }
 }
 
+fn get_or_create_doc_path() -> std::path::PathBuf {
+    let watch_path = {
+        let mut path = tauri::api::path::document_dir().expect("Could not find Document Directory");
+        path.push("Encounter Manager");
+        if !path.clone().exists() {
+            std::fs::create_dir(path.clone()).expect("Could not create directory");
+        }
+        path
+    };
+    watch_path
+}
 
 #[derive(Serialize)]
 pub struct ExampleStruct {
