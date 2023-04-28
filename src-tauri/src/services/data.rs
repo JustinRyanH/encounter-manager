b935ac5dcb08f4ab3af9d022cde5794695e33417
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-27 22:09:33 -0500

    ref: Move to file watcher

--- src-tauri/src/services/data.rs
+++ src-tauri/src/services/data.rs
@@ -5,56 +5,7 @@ use serde::Serialize;
 use tauri::{async_runtime, Manager, Runtime, State, Wry};
 use tokio::sync::{broadcast, Mutex};
 
-#[derive(Clone, Debug, Copy, Serialize)]
-pub enum FileChangEvent {
-    Create,
-    Delete,
-    Modify,
-    Rename,
-    Ignore,
-}
-
-impl From<&notify::Event> for FileChangEvent {
-    fn from(value: &notify::Event) -> Self {
-        match value.kind {
-            notify::EventKind::Any => Self::Ignore,
-            notify::EventKind::Create(_) => Self::Create,
-            notify::EventKind::Modify(ModifyKind::Name(_)) => Self::Rename,
-            notify::EventKind::Modify(_) => Self::Modify,
-            notify::EventKind::Remove(_) => Self::Delete,
-            notify::EventKind::Other => FileChangEvent::Ignore,
-            notify::EventKind::Access(_) => FileChangEvent::Ignore,
-        }
-    }
-}
-
-struct FileWatcher {
-    pub watcher: notify::RecommendedWatcher,
-}
-
-impl FileWatcher {
-    pub fn new() -> Result<(Self, broadcast::Receiver<FileChangEvent>), String> {
-        let (sender, receiver) = broadcast::channel(4);
-        let watcher = notify::recommended_watcher(move |res| {
-            match res {
-                Ok(event) => {
-                    println!("event: {:?}", event);
-                    let event = FileChangEvent::from(&event);
-                    sender.send(event).expect("Could not send event");
-                }
-                Err(e) => println!("watch error: {:?}", e),
-            };
-        })
-        .map_err(|e| e.to_string())?;
-        Ok((Self { watcher }, receiver))
-    }
-
-    pub fn watch(&mut self, path: &Path) -> Result<(), String> {
-        self.watcher
-            .watch(path, RecursiveMode::Recursive)
-            .map_err(|e| e.to_string())
-    }
-}
+use crate::services::file_watcher::FileWatcher;
 
 #[derive(Clone)]
 pub struct BackgroundData<R: Runtime> {
---
+++ src-tauri/src/services/file_watcher.rs
@@ -0,0 +1,56 @@
+use tokio::sync::broadcast;
+use std::path::Path;
+use notify::{RecursiveMode, Watcher};
+use serde::Serialize;
+use notify::event::ModifyKind;
+
+pub struct FileWatcher {
+    pub watcher: notify::RecommendedWatcher,
+}
+
+impl FileWatcher {
+    pub fn new() -> Result<(Self, broadcast::Receiver<FileChangEvent>), String> {
+        let (sender, receiver) = broadcast::channel(4);
+        let watcher = notify::recommended_watcher(move |res| {
+            match res {
+                Ok(event) => {
+                    println!("event: {:?}", event);
+                    let event = FileChangEvent::from(&event);
+                    sender.send(event).expect("Could not send event");
+                }
+                Err(e) => println!("watch error: {:?}", e),
+            };
+        })
+        .map_err(|e| e.to_string())?;
+        Ok((Self { watcher }, receiver))
+    }
+
+    pub fn watch(&mut self, path: &Path) -> Result<(), String> {
+        self.watcher
+            .watch(path, RecursiveMode::Recursive)
+            .map_err(|e| e.to_string())
+    }
+}
+
+#[derive(Clone, Debug, Copy, Serialize)]
+pub enum FileChangEvent {
+    Create,
+    Delete,
+    Modify,
+    Rename,
+    Ignore,
+}
+
+impl From<&notify::Event> for FileChangEvent {
+    fn from(value: &notify::Event) -> Self {
+        match value.kind {
+            notify::EventKind::Any => Self::Ignore,
+            notify::EventKind::Create(_) => Self::Create,
+            notify::EventKind::Modify(ModifyKind::Name(_)) => Self::Rename,
+            notify::EventKind::Modify(_) => Self::Modify,
+            notify::EventKind::Remove(_) => Self::Delete,
+            notify::EventKind::Other => FileChangEvent::Ignore,
+            notify::EventKind::Access(_) => FileChangEvent::Ignore,
+        }
+    }
+}
