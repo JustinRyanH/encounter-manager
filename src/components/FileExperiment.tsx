ab58175fe9fc69cfa4244d4e3c5e27c37c4f535a
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-29 01:01:40 -0500

    feat: Remove  code from bad direction

--- src/components/FileExperiment.tsx
+++ src/components/FileExperiment.tsx
@@ -1,33 +1,15 @@
 import React from "react";
-import { Stack, Title, Text } from "@mantine/core";
+import { Stack } from "@mantine/core";
 
 import { useFileManager } from "~/components/FileManager";
-import { useWatchValueObserver } from "~/hooks/watchValueObserver";
 
-function Directory({ name }: { name: string }) {
-    const fileManager = useFileManager();
-    const directory = React.useMemo(() => fileManager.getDirectory(name), [fileManager, name]);
-    const files = useWatchValueObserver(directory);
-    React.useEffect(() => {
-        console.log({ files, name, directory });
-    }, [files, name]);
-
-
-    return <Stack>
-        <Title order={4}>{name}</Title>
-        {files.map((file) => <Text key={file.path}>{file.name}</Text>)}
-    </Stack>
-}
 
 export function FileExperiment() {
     const fileManager = useFileManager();
-    const rootDirectory = useWatchValueObserver(fileManager.rootNamePublisher);
     React.useEffect(() => {
         fileManager.startWatching();
-        fileManager.getRoot();
     }, [fileManager]);
 
     return <Stack>
-        <Directory name={rootDirectory} />
     </Stack>
 }
--- src/services/FileManager.ts
+++ src/services/FileManager.ts
@@ -16,9 +16,13 @@ export class BaseFileManager {
     }
 }
 
+class File { }
+class Directory {
+    #files = new ValueObserver<File[]>([]);
+    #directories = new ValueObserver<Directory[]>([]);
+}
+
 export class TauriFileManager extends BaseFileManager {
-    #directories = new Map<string, ValueObserver<FileData[]>>();
-    #rootName = new ValueObserver<string>('');
     #connection;
 
     constructor() {
@@ -29,42 +33,7 @@ export class TauriFileManager extends BaseFileManager {
         });
     }
 
-    get rootName() {
-        return this.#rootName.value;
-    }
-
-    get rootNamePublisher(): ReadonlyValueObserver<string> {
-        return this.#rootName.readonly;
-    }
-
-    getDirectory(name: string): ReadonlyValueObserver<FileData[]> {
-        return this.getDirectoryObserver(name).readonly;
-    }
-
     async startWatching() {
         this.#connection.start();
-    }
-
-    async getRoot() {
-        const result = await queryRootDirectory();
-        if (result.type !== 'directory') throw new Error("Root is not a directory");
-        if (!result.data.name) throw new Error("Root directory has no name");
-        const directoryName = result.data.name;
-        const entries = result.entries ?? [];
-        this.updateDirectory(directoryName, entries);
-        console.log(entries);
-        this.#rootName.updateValue(directoryName);
-    }
-
-    private updateDirectory(name: string, entries: FileData[]) {
-        this.getDirectoryObserver(name).updateValue(entries);
-    }
-
-    private getDirectoryObserver(name: string) {
-        let observer = this.#directories.get(name);
-        if (observer) return observer;
-        observer = new ValueObserver<FileData[]>([]);
-        this.#directories.set(name, observer);
-        return observer;
     }
 }
