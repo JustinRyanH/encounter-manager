5fdcdb4b92642cca9d1640e13df5241ebc6d0441
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-29 00:35:15 -0500

    fix: always reset the value when observer changes

--- src/hooks/watchValueObserver.tsx
+++ src/hooks/watchValueObserver.tsx
@@ -5,9 +5,10 @@ import { ValueChangeMessageProps, ReadonlyValueObserver } from "~/services/Value
 export function useWatchValueObserver<T>(observer: ReadonlyValueObserver<T>): T {
     const [value, setValue] = React.useState(observer.value);
     React.useEffect(() => {
+        setValue(observer.value);
         const onChangeValue = ({ newValue }: ValueChangeMessageProps<T>) => { setValue(newValue); }
         observer.add(onChangeValue);
         return () => observer.remove(onChangeValue);
-    });
+    }, [observer]);
     return value;
 }
