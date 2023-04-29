6347eb826f82f48c0b345579fba4dcef3829d6df
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-28 22:46:12 -0500

    ref: Remove dead code

--- src/components/FileExperiment.tsx
+++ src/components/FileExperiment.tsx
@@ -1,39 +1,8 @@
 import React from "react";
 import { Button, Stack } from "@mantine/core";
 import { invoke } from "@tauri-apps/api";
-import { listen, UnlistenFn } from "@tauri-apps/api/event";
-import { v4 } from "uuid";
+
 import { useFileManager } from "~/components/FileManager";
-import { on } from "process";
-
-class ListenSingleton {
-    private static instance: ListenSingleton;
-    #stopListening: UnlistenFn | null = null;
-    #uuid;
-
-    static get(): ListenSingleton {
-        if (!ListenSingleton.instance) {
-            ListenSingleton.instance = new ListenSingleton();
-        }
-        return ListenSingleton.instance;
-    }
-
-    private constructor() {
-        this.#uuid = v4();
-        this.setupListener();
-    }
-
-    get uuid() {
-        return this.#uuid;
-    }
-
-    private async setupListener() {
-        this.#stopListening = await listen(
-            "test",
-            (event) => console.log(this.uuid, event)
-        );
-    }
-}
 
 export function FileExperiment() {
     const fileManager = useFileManager();
