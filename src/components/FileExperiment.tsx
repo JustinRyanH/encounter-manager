eb9099f506574e0aa494a837530a4ddd2cabdf92
Justin Hurstwright <jryanhurst@gmail.com>
2023-04-30 19:56:09 -0500

    style: Let the text overflows

--- src/components/FileExperiment.tsx
+++ src/components/FileExperiment.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Group, Stack, Text } from "@mantine/core";
+import { Button, Flex, Stack, Text } from "@mantine/core";
 import { File as FileIcon, FolderNotch, Plus } from '@phosphor-icons/react';
 
 import { useFileManager } from "~/components/FileManager";
@@ -9,25 +9,23 @@ import { useWatchValueObserver } from "~/hooks/watchValueObserver";
 
 function FileLine({ file, spacing }: { file: File, spacing: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '' }) {
     const name = useWatchValueObserver(file.nameObserver);
-    return (<Group>
+    return (<Flex gap="xs" justify="space-between" align="center" wrap="nowrap">
         <FileIcon />
         <Text pl={spacing} size="sm">{name}</Text>
-    </Group>)
+    </Flex>)
 }
 
 function DirectoryLine({ directory }: { directory: Directory }) {
     const name = useWatchValueObserver(directory.nameObserver);
     const files = useWatchValueObserver(directory.filesObserver);
     return (<>
-        <Group position="apart">
-            <Group position="left">
-                <FolderNotch />
-                <Text size="sm">{name}</Text>
-            </Group>
+        <Flex gap="xs" justify="space-between" align="center" wrap="nowrap">
+            <FolderNotch style={{ minWidth: "1rem" }} />
+            <Text style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', flexGrow: 1, overflow: 'hidden' }} size="sm">{name}</Text>
             <Button px="xs" size="xs" variant="subtle" color="gray" disabled={!files.length}>
                 <Plus />
             </Button>
-        </Group>
+        </Flex>
         {files.map((file) => <FileLine key={file.path} file={file} spacing="xs" />)}
     </>);
 }
