09d2e88207d782aee6bc3b4852efb7e4b2959145
Justin Hurstwright <jryanhurst@gmail.com>
2023-05-07 12:36:11 -0500

    feat: Add in Character Sorting

--- src-tauri/src/encounters/character.rs
+++ src-tauri/src/encounters/character.rs
@@ -1,23 +1,24 @@
+use std::cmp::Ordering;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Copy, Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct Hitpoints {
     pub current: i32,
     pub max: i32,
     pub temporary: i32,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct Character {
     ulid: ulid::Ulid,
     pub name: String,
     pub hp: Hitpoints,
     pub initiative: i32,
-    pub initiative_match_score: Option<i32>,
+    pub initiative_match_score: i32,
 }
 
 impl Character {
-    pub fn new(name: String, max_hp: i32, initiative: i32) -> Character {
+    pub fn new<T: Into<String>>(name: T, max_hp: i32, initiative: i32) -> Character {
         let hp = Hitpoints {
             current: max_hp,
             max: max_hp,
@@ -25,21 +26,55 @@ impl Character {
         };
         Character {
             ulid: ulid::Ulid::new(),
-            name,
+            name: name.into(),
             hp,
             initiative,
-            initiative_match_score: None,
+            initiative_match_score: 0,
         }
     }
 
     pub fn id(&self) -> String {
         self.ulid.to_string()
     }
+
+    pub fn is_same_as(&self, other: &Character) -> bool {
+        self.id() == other.id()
+    }
+
+    pub fn is_similar_to(&self, other: &Character) -> bool {
+        self.name == other.name
+    }
+
+    pub fn set_initiative_match_score(&mut self, score: i32) {
+        self.initiative_match_score = score;
+    }
+}
+
+
+impl PartialOrd for Character {
+    fn partial_cmp(&self, other: &Character) -> Option<std::cmp::Ordering> {
+        match other.initiative.partial_cmp(&self.initiative) {
+            Some(std::cmp::Ordering::Equal) => other
+                .initiative_match_score
+                .partial_cmp(&self.initiative_match_score),
+            Some(ordering) => Some(ordering),
+            None => None,
+        }
+    }
+}
+
+impl Ord for Character {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.partial_cmp(other) {
+            Some(ordering) => ordering,
+            None => Ordering::Equal,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::encounters::*;
+    use serde::de::Unexpected::Char;
     use crate::encounters::character::Character;
 
     #[test]
@@ -53,7 +88,61 @@ mod tests {
         assert_eq!(character.hp.max, max_hp);
         assert_eq!(character.hp.temporary, 0);
         assert_eq!(character.initiative, initiative);
-        assert_eq!(character.initiative_match_score, None);
+        assert_eq!(character.initiative_match_score, 0);
         assert_eq!(character.id().len(), 26);
+    }
+
+    #[test]
+    fn test_is_same_as() {
+        let name = String::from("Test Character");
+        let max_hp = 10;
+        let initiative = 10;
+        let character = Character::new(name.clone(), max_hp, initiative);
+        let other_character = Character::new(name.clone(), max_hp, initiative);
+        let cloned_character = character.clone();
+        assert_eq!(character.is_same_as(&other_character), false);
+        assert_eq!(character.is_same_as(&cloned_character), true);
+    }
+
+    #[test]
+    fn test_is_similar_to() {
+        let name = String::from("Test Character");
+        let max_hp = 10;
+        let initiative = 10;
+        let character = Character::new(name.clone(), max_hp, initiative);
+        let other_character = Character::new(name.clone(), max_hp, initiative);
+        let cloned_character = character.clone();
+        assert_eq!(character.is_similar_to(&other_character), true);
+        assert_eq!(character.is_similar_to(&cloned_character), true);
+    }
+
+    #[test]
+    fn test_partial_ordering() {
+        let max_hp = 10;
+        let character_a = Character::new("character a", max_hp, 10);
+        let character_b = Character::new("character b", max_hp, 9);
+        let character_c = Character::new("character c", max_hp, 11);
+        let mut character_d = Character::new("character d", max_hp, 10);
+        let mut character_e = Character::new("character e", max_hp, 10);
+        character_d.set_initiative_match_score(18);
+        character_e.set_initiative_match_score(5);
+
+        let expected_order: [Character; 5] = [
+            character_c.clone(),
+            character_d.clone(),
+            character_e.clone(),
+            character_a.clone(),
+            character_b.clone(),
+        ];
+
+        let mut characters = vec![
+            character_a.clone(),
+            character_b.clone(),
+            character_c.clone(),
+            character_d.clone(),
+            character_e.clone(),
+        ];
+        characters.sort();
+        assert_eq!(characters, expected_order.to_vec());
     }
 }
