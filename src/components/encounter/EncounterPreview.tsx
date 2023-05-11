427e86a1e6f5a62d7bdd7a142479cb4cb8f2dc57
Justin Hurstwright <jryanhurst@gmail.com>
2023-05-11 08:15:13 -0500

    ref: Format

--- src/components/encounter/EncounterPreview.tsx
+++ src/components/encounter/EncounterPreview.tsx
@@ -2,7 +2,6 @@ import { ActiveCharacter, Encounter } from "~/services/encounter";
 import { useWatchValueObserver } from "~/hooks/watchValueObserver";
 import { Accordion, AccordionControlProps, ActionIcon, Avatar, Badge, Box, Group } from "@mantine/core";
 import { BookOpen } from '@phosphor-icons/react';
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
 interface ControlProps extends AccordionControlProps {
@@ -13,21 +12,19 @@ function AccordionControl({ encounter, ...props }: ControlProps) {
   const navigate = useNavigate()
   const encounterName = useWatchValueObserver(encounter.nameObserver);
 
+  const onClick = () => navigate(`encounter/${encounter.id}`);
   return (<Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <Accordion.Control {...props} />
-      <ActionIcon onClick={() => {
-        console.log('click');
-        navigate(`encounter/${encounter.id}`);
-      }} variant="outline" size="lg" color="grape" title={`Open ${encounterName} Encounter`}>
-        <BookOpen size="1rem"/>
-      </ActionIcon>
-    </Box>
+    <Accordion.Control {...props} />
+    <ActionIcon onClick={onClick} variant="outline" size="lg" color="grape" title={`Open ${encounterName} Encounter`}>
+      <BookOpen size="1rem" />
+    </ActionIcon>
+  </Box>
   )
 }
 
 function EncounterPreviewCharacter({ character }: { character: ActiveCharacter }) {
   const name = useWatchValueObserver(character.nameObserver);
-  const avatar = <Avatar size={32} radius="xl"/>;
+  const avatar = <Avatar size={32} radius="xl" />;
 
   return <Badge leftSection={avatar} size="lg"> {name} </Badge>;
 }
@@ -35,8 +32,7 @@ function EncounterPreviewCharacter({ character }: { character: ActiveCharacter }
 export function EncounterPreview({ encounter }: { encounter: Encounter }) {
   const name = useWatchValueObserver(encounter.nameObserver);
   const characters = useWatchValueObserver(encounter.charactersObserver);
-  const listedCharacters = characters.map((character) => <EncounterPreviewCharacter character={character}
-                                                                                    key={character.id}/>);
+  const listedCharacters = characters.map((character) => <EncounterPreviewCharacter character={character} key={character.id} />);
   return <div>
     <Accordion.Item value={encounter.id}>
       <AccordionControl encounter={encounter}>{name}</AccordionControl>
