40c21490dd8d02a21d0e91d3692d7560c8ba3237
Justin Hurstwright <jryanhurst@gmail.com>
2023-05-10 21:24:41 -0500

    ref: Move component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import React from "react";
-import { Accordion, AppShell, } from "@mantine/core";
+import { AppShell, } from "@mantine/core";
 
 import { AppHeader } from "~/components/AppHeader";
 
 import "./App.css";
-import { notifyErrors } from "~/services/notifications";
 import { EncounterProvider } from "~/components/encounter/EncounterContext";
-import { useEncounterManager } from "~/components/encounter/EncounterManagerProvider";
-import { useWatchValueObserver } from "~/hooks/watchValueObserver";
-import { EncounterPreview } from "~/components/encounter/EncounterPreview";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
-
-function EncounterList() {
-    const encounterManager = useEncounterManager();
-    const encounterIds = useWatchValueObserver(encounterManager.encountersObserver);
-    const encounters = React.useMemo(() => encounterManager.encounters, [encounterIds]);
-    React.useEffect(() => {
-        encounterManager.refreshList().catch(notifyErrors);
-    }, [encounterManager]);
-
-    return (
-        <Accordion>
-            {encounters.map((encounter) => (<EncounterPreview encounter={encounter} key={encounter.id}/>))}
-        </Accordion>
-    );
-}
+import { EncounterList } from "~/components/encounter/EncounterList";
 
 function App() {
     return (<AppShell header={<AppHeader/>}>
---
+++ src/components/encounter/EncounterList.tsx
@@ -0,0 +1,21 @@
+import { useEncounterManager } from "~/components/encounter/EncounterManagerProvider";
+import { useWatchValueObserver } from "~/hooks/watchValueObserver";
+import React from "react";
+import { notifyErrors } from "~/services/notifications";
+import { Accordion } from "@mantine/core";
+import { EncounterPreview } from "~/components/encounter/EncounterPreview";
+
+export function EncounterList() {
+    const encounterManager = useEncounterManager();
+    const encounterIds = useWatchValueObserver(encounterManager.encountersObserver);
+    const encounters = React.useMemo(() => encounterManager.encounters, [encounterIds]);
+    React.useEffect(() => {
+        encounterManager.refreshList().catch(notifyErrors);
+    }, [encounterManager]);
+
+    return (
+        <Accordion>
+            {encounters.map((encounter) => (<EncounterPreview encounter={encounter} key={encounter.id}/>))}
+        </Accordion>
+    );
+}
