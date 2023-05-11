245b3884057f6ec307e62b46cb395085025d88a8
Justin Hurstwright <jryanhurst@gmail.com>
2023-05-10 21:24:22 -0500

    feat: Enable Routers

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,33 @@ import { EncounterProvider } from "~/components/encounter/EncounterContext";
 import { useEncounterManager } from "~/components/encounter/EncounterManagerProvider";
 import { useWatchValueObserver } from "~/hooks/watchValueObserver";
 import { EncounterPreview } from "~/components/encounter/EncounterPreview";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 function EncounterList() {
-  const encounterManager = useEncounterManager();
-  const encounterIds = useWatchValueObserver(encounterManager.encountersObserver);
-  const encounters = React.useMemo(() => encounterManager.encounters, [encounterIds]);
-  React.useEffect(() => {
-    encounterManager.refreshList().catch(notifyErrors);
-  }, [encounterManager]);
+    const encounterManager = useEncounterManager();
+    const encounterIds = useWatchValueObserver(encounterManager.encountersObserver);
+    const encounters = React.useMemo(() => encounterManager.encounters, [encounterIds]);
+    React.useEffect(() => {
+        encounterManager.refreshList().catch(notifyErrors);
+    }, [encounterManager]);
 
-  return (
-    <Accordion>
-      {encounters.map((encounter) => (<EncounterPreview encounter={encounter} key={encounter.id} />))}
-    </Accordion>
-  );
+    return (
+        <Accordion>
+            {encounters.map((encounter) => (<EncounterPreview encounter={encounter} key={encounter.id}/>))}
+        </Accordion>
+    );
 }
 
 function App() {
-  return (<AppShell header={<AppHeader />}>
-    <EncounterProvider>
-      <EncounterList />
-    </EncounterProvider>
-  </AppShell>);
+    return (<AppShell header={<AppHeader/>}>
+        <EncounterProvider>
+            <MemoryRouter initialEntries={['/encounters']}>
+                <Routes>
+                    <Route path="/encounters" element={<EncounterList/>}/>
+                </Routes>
+            </MemoryRouter>
+        </EncounterProvider>
+    </AppShell>);
 }
 
 export default App;
