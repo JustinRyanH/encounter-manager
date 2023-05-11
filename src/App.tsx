448f046efb5f9c01ceac12523b81391a7f940526
justinhurstwright <jryanhurst@gmail.com>
2023-05-10 20:29:49 -0500

    feat: Display batches

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Accordion, AppShell, List, Stack } from "@mantine/core";
+import { Accordion, AppShell, Avatar, Badge, Group, } from "@mantine/core";
 
 import { AppHeader } from "~/components/AppHeader";
 
@@ -11,46 +11,48 @@ import { useWatchValueObserver } from "~/hooks/watchValueObserver";
 import { ActiveCharacter, Encounter } from "~/services/encounter";
 
 function EncounterPreviewCharacter({ character }: { character: ActiveCharacter }) {
-    const name = useWatchValueObserver(character.nameObserver);
+  const name = useWatchValueObserver(character.nameObserver);
+  const avatar = <Avatar size={32} radius="xl" />;
 
-    return <List.Item>{name}</List.Item>
+  return <Badge leftSection={avatar} size="lg"> {name} </Badge >;
 }
+
 function EncounterPreview({ encounter }: { encounter: Encounter }) {
-    const name = useWatchValueObserver(encounter.nameObserver);
-    const characters = useWatchValueObserver(encounter.charactersObserver);
-    const listedCharacters = characters.map((character) => <EncounterPreviewCharacter character={character} key={character.id} />);
-    return <div>
-        <Accordion.Item value={encounter.id}>
-            <Accordion.Control>{name}</Accordion.Control>
-            <Accordion.Panel>
-                <List>
-                    {listedCharacters}
-                </List>
-            </Accordion.Panel>
-        </Accordion.Item>
-    </div>
+  const name = useWatchValueObserver(encounter.nameObserver);
+  const characters = useWatchValueObserver(encounter.charactersObserver);
+  const listedCharacters = characters.map((character) => <EncounterPreviewCharacter character={character} key={character.id} />);
+  return <div>
+    <Accordion.Item value={encounter.id}>
+      <Accordion.Control>{name}</Accordion.Control>
+      <Accordion.Panel>
+        <Group spacing="xs">
+          {listedCharacters}
+        </Group>
+      </Accordion.Panel>
+    </Accordion.Item>
+  </div>
 }
 
 function EncounterList() {
-    const encounterManager = useEncounterManager();
-    const encounterIds = useWatchValueObserver(encounterManager.encountersObserver);
-    const encounters = React.useMemo(() => encounterManager.encounters, [encounterIds]);
-    React.useEffect(() => {
-        encounterManager.refreshList().catch(notifyErrors);
-    }, [encounterManager]);
+  const encounterManager = useEncounterManager();
+  const encounterIds = useWatchValueObserver(encounterManager.encountersObserver);
+  const encounters = React.useMemo(() => encounterManager.encounters, [encounterIds]);
+  React.useEffect(() => {
+    encounterManager.refreshList().catch(notifyErrors);
+  }, [encounterManager]);
 
-    return (
-        <Accordion>
-            {encounters.map((encounter) => (<EncounterPreview encounter={encounter} key={encounter.id} />))}
-        </Accordion>
-    );
+  return (
+    <Accordion>
+      {encounters.map((encounter) => (<EncounterPreview encounter={encounter} key={encounter.id} />))}
+    </Accordion>
+  );
 }
 
 function App() {
   return (<AppShell header={<AppHeader />}>
-      <EncounterProvider>
-          <EncounterList />
-      </EncounterProvider>
+    <EncounterProvider>
+      <EncounterList />
+    </EncounterProvider>
   </AppShell>);
 }
 
