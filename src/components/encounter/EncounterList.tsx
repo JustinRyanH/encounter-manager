373f08925ebbf142fb545a6b718c808e3337cc72
Justin Hurstwright <jryanhurst@gmail.com>
2023-05-12 10:02:13 -0500

    ref: Whitespace

--- src/components/encounter/EncounterList.tsx
+++ src/components/encounter/EncounterList.tsx
@@ -1,28 +1,22 @@
+import React from "react";
+import { Accordion } from "@mantine/core";
+
 import { useEncounterManager } from "~/components/encounter/EncounterManagerProvider";
 import { useWatchValueObserver } from "~/hooks/watchValueObserver";
-import React from "react";
 import { notifyErrors } from "~/services/notifications";
-import { Accordion } from "@mantine/core";
 import { EncounterPreview } from "~/components/encounter/EncounterPreview";
+import { Encounter } from "~/services/encounter";
 
 export function EncounterList() {
   const encounterManager = useEncounterManager();
-  const encounterIds = useWatchValueObserver(
-    encounterManager.encountersObserver
-  );
-  const encounters = React.useMemo(
-    () => encounterManager.encounters,
-    [encounterIds]
-  );
+  const encounterIds = useWatchValueObserver(encounterManager.encountersObserver);
+  const encounters = React.useMemo(() => encounterManager.encounters, [encounterIds]);
   React.useEffect(() => {
     encounterManager.refreshList().catch(notifyErrors);
   }, [encounterManager]);
 
-  return (
-    <Accordion>
-      {encounters.map((encounter) => (
-        <EncounterPreview encounter={encounter} key={encounter.id} />
-      ))}
-    </Accordion>
-  );
+  const formatPreview = (encounter: Encounter) => <EncounterPreview encounter={encounter} key={encounter.id} />;
+  const previews = encounters.map(formatPreview);
+
+  return <Accordion> {previews} </Accordion>;
 }
