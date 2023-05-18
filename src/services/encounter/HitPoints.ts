820b020095489e03903d12d99df5546e17ef256e
Justin Hurstwright <jryanhurst@gmail.com>
2023-05-18 09:14:19 -0500

    ref: remove dead code

--- src/services/encounter/HitPoints.ts
+++ src/services/encounter/HitPoints.ts
@@ -12,22 +12,6 @@ export interface HitPointsProps {
  * A class that represents a character's hit points.
  */
 export class HitPoints {
-  static ValidateTotal = (value: number | null): string[] => {
-    if (!value) return ["Total hit points cannot be empty"];
-    if (value < 0) return ["Total hit points cannot be negative"];
-    return [];
-  };
-  static ValidateCurrent = (value: number | null): string[] => {
-    if (!value) return ["Current hit points cannot be empty"];
-    return [];
-  };
-
-  static ValidateTemp = (value: number | null): string[] => {
-    if (value === null) return [];
-    if (value < 0) return ["Temporary hit points cannot be negative"];
-    return [];
-  };
-
   #total: ValueObserver<number> = new ValueObserver(0);
   #current: ValueObserver<number> = new ValueObserver(0);
   #temp: ValueObserver<number> = new ValueObserver<number>(0);
@@ -106,77 +90,4 @@ export class HitPoints {
   get tempObserver(): ReadonlyValueObserver<number> {
     return this.#temp.readonly;
   }
-
-  /**
-   * Update current hit points, and notify observers
-   * @param value
-   */
-  setCurrent = (value: number) => {
-    this.current = value;
-  };
-
-  /**
-   * Update total hit points, and notify observers
-   * @param value
-   */
-  setTotal = (value: number | null) => {
-    const errors = this.#validateTotal(value).join(", ");
-    if (notifyErrors({ errors, title: "Invalid Hit Points" })) return;
-    if (!value) return;
-
-    this.total = value;
-    if (this.current > value) {
-      this.current = value;
-    }
-  };
-
-  /**
-   * Update temporary hit points, and notify observers
-   */
-  setTemp = (value: number | null) => {
-    const errors = this.#validateTemp(value).join(", ");
-    if (notifyErrors({ errors, title: "Invalid Temporary Hit Points" })) return;
-
-    if (value === null) {
-      if (this.temporary === 0) return;
-      this.temporary = 0;
-      notifications.show({
-        title: "Reset Temporary Hit Points",
-        message: "Zeroed out temporary hit points",
-        color: "blue",
-      });
-      return;
-    }
-    this.temporary = Math.max(0, value);
-  };
-
-  /**
-   * Damage the character and notify observers
-   *
-   * If the character has temporary hit points, damage those first then the current
-   * @param amount
-   */
-  damage = (amount: number) => {
-    const leftOver = this.temporary - amount;
-    if (leftOver >= 0) {
-      this.temporary -= amount;
-      return;
-    }
-
-    this.temporary = 0;
-    this.current = Math.max(this.current - Math.abs(leftOver), 0);
-  };
-
-  /**
-   * Heal the character but not above the total hit points and notify observers
-   * @param amount
-   */
-  heal = (amount: number) => {
-    const newAmount = this.current + amount;
-    this.current = Math.min(newAmount, this.total);
-  };
-
-  #validateTotal = HitPoints.ValidateTotal;
-  #validateCurrent = HitPoints.ValidateCurrent;
-  #validateTemp = HitPoints.ValidateTemp;
 }
