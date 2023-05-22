1546765835a9abef950ffd561bf6cbea8f0ca79c
Justin Hurstwright <jryanhurst@gmail.com>
2023-05-22 10:57:09 -0500

    ref: move hook

--- src/components/encounter/display/character/ViewEncounterControl.tsx
+++ src/components/encounter/display/character/ViewEncounterControl.tsx
@@ -1,9 +1,8 @@
 import { ArrowBendRightDown } from "@phosphor-icons/react";
 import { Accordion, AccordionControlProps, ActionIcon, Box, MantineTheme, Paper } from "@mantine/core";
-
-import { useWatchValueObserver } from "~/hooks/watchValueObserver";
 import { EncounterCharacter, ViewEncounter } from "~/services/encounter";
 import { useEncounterContext } from "~/components/encounter/providers/EncounterProvider";
+import { useInPlayForEncounter } from "~/components/encounter/hooks";
 
 const ViewSx = (theme: MantineTheme) => ({
   display: "flex",
@@ -23,12 +22,6 @@ const NextButtonSx = {
 interface EncounterControlProps extends AccordionControlProps {
   character: EncounterCharacter;
   view: ViewEncounter;
-}
-
-function useInPlayForEncounter(character: EncounterCharacter) {
-  const encounter = useEncounterContext();
-  const activeCharacter = useWatchValueObserver(encounter.activeCharacterObserver);
-  return activeCharacter?.id === character.id;
 }
 
 function NextCharacterButton({ character }: { character: EncounterCharacter }) {
--- src/components/encounter/hooks.ts
+++ src/components/encounter/hooks.ts
@@ -1,6 +1,9 @@
 import { useEncounterManager } from "~/components/encounter/providers/EncounterManagerProvider";
 import React from "react";
 import { notifyErrors } from "~/services/notifications";
+import { EncounterCharacter } from "~/services/encounter";
+import { useEncounterContext } from "~/components/encounter/providers/EncounterProvider";
+import { useWatchValueObserver } from "~/hooks/watchValueObserver";
 
 export function useRefreshEncounter() {
   const encounterManager = useEncounterManager();
@@ -8,4 +11,10 @@ export function useRefreshEncounter() {
     encounterManager.refreshList().catch(notifyErrors);
   }, [encounterManager]);
   return encounterManager;
+}
+
+export function useInPlayForEncounter(character: EncounterCharacter) {
+  const encounter = useEncounterContext();
+  const activeCharacter = useWatchValueObserver(encounter.activeCharacterObserver);
+  return activeCharacter?.id === character.id;
 }
