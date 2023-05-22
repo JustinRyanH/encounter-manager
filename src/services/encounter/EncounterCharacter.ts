8877ee75da1a3818263b77d1362ff7e39731f036
Justin Hurstwright <jryanhurst@gmail.com>
2023-05-22 09:37:29 -0500

    reF: Duplicate code

--- src/services/encounter/EncounterCharacter.ts
+++ src/services/encounter/EncounterCharacter.ts
@@ -30,7 +30,107 @@ interface EncounterCharacterUpdateProps extends OptionalCharacterProps {
   hp?: OptionalHitPoints;
 }
 
-export class Character {}
+export class Character {
+  readonly id: string;
+  readonly isStub: boolean;
+  readonly hp: HitPoints = new HitPoints();
+  #name: ValueObserver<string>;
+  #initiative: ValueObserver<number>;
+
+  constructor({ id, name, initiative, hp, isStub = false }: EncounterCreateProps) {
+    this.id = id;
+    this.isStub = isStub;
+    this.#initiative = new ValueObserver(initiative);
+    this.#name = new ValueObserver(name);
+    if (hp) {
+      this.hp = new HitPoints(hp);
+    }
+  }
+
+  /**
+   * The name of the character
+   */
+  get name(): string {
+    return this.#name.value;
+  }
+
+  /**
+   * Update name of the character, and notify observers
+   * @param name
+   */
+  set name(name: string) {
+    this.#name.value = name;
+  }
+
+  /**
+   * Observer for the name of the character
+   */
+  get nameObserver(): ReadonlyValueObserver<string> {
+    return this.#name.readonly;
+  }
+
+  /**
+   * The initiative of the character
+   */
+  get initiative(): number {
+    return this.#initiative.value;
+  }
+
+  /**
+   * Update initiative of the character, and notify observers
+   * @param initiative
+   */
+  set initiative(initiative: number) {
+    this.#initiative.value = initiative;
+  }
+
+  /**
+   * Observer for the initiative of the character
+   */
+  get initiativeObserver(): ReadonlyValueObserver<number> {
+    return this.#initiative.readonly;
+  }
+
+  update(values: EncounterCharacterUpdateProps) {
+    if (values.id !== this.id) throw new Error("Id Mismatch for character");
+    if (values.name && values.name !== this.name) this.name = values.name;
+    if (values.initiative && values.initiative !== this.initiative) this.initiative = values.initiative;
+    if (values.hp) {
+      const { current, total, temporary } = values.hp;
+      if (current !== undefined && current !== this.hp.current) this.hp.current = current;
+      if (total !== undefined && total !== this.hp.total) this.hp.total = total;
+      if (temporary !== undefined && temporary !== this.hp.temporary) this.hp.temporary = temporary;
+    }
+  }
+
+  updateInitiative = (_initiative: number | null): void => {
+    throw new Error("Not Implemented");
+  };
+
+  updateName = async (_name: string) => {
+    throw new Error("Not Implemented");
+  };
+
+  updateTotalHp = async (_total: number) => {
+    throw new Error("Not Implemented");
+  };
+
+  updateCurrentHp = async (_current: number) => {
+    throw new Error("Not Implemented");
+  };
+
+  updateTempHp = async (_temp: number | null) => {
+    throw new Error("Not Implemented");
+  };
+
+  heal = async (_amount: number) => {
+    throw new Error("Not Implemented");
+  };
+
+  damage = async (_amount: number) => {
+    throw new Error("Not Implemented");
+  };
+}
 
 /**
  * A Tracked Character
